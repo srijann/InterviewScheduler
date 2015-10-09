99e2ca2e5150d48b1dbf191a6e6827df5db73976
JavaSlasher <nirishpyakurel@gmail.com>
2015-10-09 11:14:52 -0600

    Changes Upto data UI
    Get it man!!!!!

--- src/main/webapp/resources/js/services/consultantService.js
+++ src/main/webapp/resources/js/services/consultantService.js
@@ -1,8 +1,6 @@
 'use strict';
-
 /* Services */
-
-app.service("consultantService", ['$http', '$q', function($http, $q){
+interviewSchedulerApp.service('consultantService', ['$http', '$q', function($http, $q){
 	return {
 		fetchAllConsultants : function(){
 			return $http.get('http://localhost:8080/InterviewScheduler/consultants')
@@ -17,7 +15,37 @@ app.service("consultantService", ['$http', '$q', function($http, $q){
 						return $q.reject(errResponse);
 					}
 			);
+		},
+		editConsultant : function(){
+			var editConsulatantData = {
+				      "id": 3,
+				      "name": "Naresh",
+				      "ssn": "443-30-8769",
+				      "dob": "1984-02-28",
+				      "email": "nrs@hotmail.com",
+				      "phone": "303-344-4799",
+				      "yrsExp": 6.5,
+				      "consultantClient": [],
+				      "educationDetail": {
+				        "id": 3,
+				        "institutionName": "Regis",
+				        "location": "Denver, CO",
+				        "graduateYear": 2009
+				      },
+				      "vendors": []
+				    };
+			console.log('cons serv : ' , editConsulatantData);
+			$http.post('http://localhost:8080/InterviewScheduler/editConsultant', editConsulatantData)
+			.success(function(editConsulatantData, status, headers, config){
+				console.log('post servie hit'+editConsulatantData);
+
+			}).error(function(editConsultantData, status, headers, config) {
+				alert("failure");
+			}
+			);	
+
 		}
+
 	}
 
 }]);
