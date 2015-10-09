963e4983ab0e288adfff1cfe63236c1a257f9b6b
JavaSlasher <nirishpyakurel@gmail.com>
2015-10-09 13:16:05 -0600

    Here we go...here we go again

--- src/main/java/com/nepal/controller/ConsultantController.java
+++ src/main/java/com/nepal/controller/ConsultantController.java
@@ -1,10 +1,18 @@
 package com.nepal.controller;
 
-import java.util.List;
+import java.io.IOException;
+import java.util.*;
+
+import com.nepal.beans.Status;
 
 import org.apache.log4j.Logger;
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,19 +25,46 @@ import com.nepal.service.PersistenceService;
 public class ConsultantController {
 
 	private static Logger logger = Logger.getLogger(ConsultantController.class);
-	
+
 	@Autowired
 	PersistenceService persistenceService;
-	
+
 	@Autowired
 	ConsultantService consultantService;
+
+	/*@RequestMapping(value = "/consultants", method = RequestMethod.GET)
+	public @ResponseBody String getConsultant() throws JsonGenerationException, JsonMappingException, IOException{
+		ObjectMapper mapper = new ObjectMapper();
+		String jsonString = mapper.writeValueAsString(consultantService.getAllConsultants());
+		System.out.println("NOT JSON: \n" + jsonString);
+		return jsonString;
+
+	}*/
 	
 	@RequestMapping(value = "/consultants", method = RequestMethod.GET)
 	public @ResponseBody List<Consultant> getConsultants(){
-		System.out.println(consultantService.getConsultantById(5L));
-		return null;
-		
+		List<Consultant> consultants = consultantService.getAllConsultants();
+		return consultants;
+
 	}
 	
-}
+	@RequestMapping(value = "/editConsultant", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody String editConsultant(@RequestBody Consultant consultant){
+		System.out.println("inside edit consultant" +  consultant);
+		return null;
+	}
+	/*@RequestMapping(value = "/addConsultant", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)  
+	 public @ResponseBody  
+	 Status addEmployee(@RequestBody Consultant consultant) {  
+	  try {  
+	   ConsultantService.addEntity(consultant);  
+	   return new Status(1, "Employee added Successfully !");  
+	  } catch (Exception e) {  
+	   // e.printStackTrace();  
+	   return new Status(0, e.toString());  
+	  }  */
+
+}  
+
+
 
--- src/main/webapp/resources/js/services/consultantService.js
+++ src/main/webapp/resources/js/services/consultantService.js
@@ -16,24 +16,7 @@ interviewSchedulerApp.service('consultantService', ['$http', '$q', function($htt
 					}
 			);
 		},
-		editConsultant : function(){
-			var editConsulatantData = {
-				      "id": 3,
-				      "name": "Naresh",
-				      "ssn": "443-30-8769",
-				      "dob": "1984-02-28",
-				      "email": "nrs@hotmail.com",
-				      "phone": "303-344-4799",
-				      "yrsExp": 6.5,
-				      "consultantClient": [],
-				      "educationDetail": {
-				        "id": 3,
-				        "institutionName": "Regis",
-				        "location": "Denver, CO",
-				        "graduateYear": 2009
-				      },
-				      "vendors": []
-				    };
+		editConsultant : function(editConsulatantData){
 			console.log('cons serv : ' , editConsulatantData);
 			$http.post('http://localhost:8080/InterviewScheduler/editConsultant', editConsulatantData)
 			.success(function(editConsulatantData, status, headers, config){
